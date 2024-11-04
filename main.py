5981d5e5ee6dda73551fdce02d40f8e407119a2d
Hossein <hossein.gasemzadeh03@gmail.com>
2024-11-04 16:46:39 +0330

    feat(web-scraper): add initial Streamlit web scraper interface
    Introduced a new web scraper application using Streamlit that allows
    users to input any URL and specify content extraction requirements.

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.title("Welcome")
+st.write('This is an AI web scraper designed to assist you in extracting valuable information from websites effortlessly.')
+
+st.write("""
+Our AI-powered web scraper allows you to input any URL and specify exactly what content you're interested in extracting from that page.
+
+### Key Features:
+- **Flexible Input**: Enter any valid URL and specify your requirements through a customizable prompt.
+- **User-Friendly Interface**: Enjoy an intuitive design that makes it easy for anyone to use the web scraper.
+- **AI Integration**: Leverage the power of artificial intelligence to extract relevant information accurately.
+
+### How It Works:
+1. **Input URL**: Start by entering the URL of the website you wish to scrape.
+2. **Define Your Request**: In the prompt section, explain what specific information you would like to retrieve. Be as detailed as possible to get the best results.
+3. **Start the Scraping Process**: Click the "search" button, and the AI will handle the rest, pulling the desired data from the provided URL.
+
+### Getting Started:
+To contribute, report issues, or explore the source code, please visit our project's GitHub repository: [GitHub - Scrape Wizard](https://github.com/hosseingz/ScrapeWizard)
+
+We are continually improving this project and welcome contributions from the community. Happy scraping!
+""")
+
+url = st.text_input("URL", placeholder='Enter the URL you want to scrape')
+prompt = st.text_area("Prompt", placeholder="What do you want from this URL?")
+
+def clicked():
+    ...
+
+st.button("search", on_click=clicked)
