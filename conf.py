519a4e73f2ff691b989eab0428a61193bccb2162
hosseingz <hossein.gasemzadeh03@gmail.com>
2024-11-22 00:34:28 +0330

    feat(ui): improve instructions and layout in main.py
    Updated the instruction text in conf.py for clarity and conciseness.
    The welcome message and key features in main.py now utilize a
    placeholder for an improved interface, enhancing user interaction
    and guiding users more effectively on how to use the AI web
    scraper. Adjusted the layout for better readability and
    understanding of the scraping process.

--- conf.py
+++ conf.py
@@ -1,19 +1,21 @@
 MODEL = "phi3:mini"
 
 TEMPLATE = """
-Answer the question based on the context below: 
+Please provide a direct answer to the question based on the context below:
 
-Context : {context}
-
-
-please follow these instructions carefully:
-
-1. **Extract Information:** Only extract the information that directly mathces the provided question:
+Context: {context}
 
 Question: {question}
 
+Instructions:
 
-2. **No Extra Content:** Do not include any additional text, comments, or explanations in your response.
-3. **Empty Response:** If no information matches the question, return an empty string ('').
-4. **Direct Data Only:** Your output should contain only the data that is explicitly requested, with no other text.
+1. Extract Relevant Information: Identify and extract only the information that directly answers the question. Focus specifically on details from the context that match the query.
+
+2. Avoid Extraneous Content: Do not include any additional explanations, comments, or surrounding narrative. Your response should be concise and to the point.
+
+3. Return Empty if No Match: If there is no relevant information that answers the question, respond with an empty string ('').
+
+4. Output Format: Ensure your response includes only the requested data without any additional text or formatting.
+
+4. Direct Data Only: Your output should contain only the data that is explicitly requested, with no other text.
 """
--- main.py
+++ main.py
@@ -2,45 +2,48 @@ import streamlit as st
 from funcs import *
 
 
+placeholder = st.empty()
 
-st.title("Welcome")
-st.write('This is an AI web scraper designed to assist you in extracting valuable information from websites effortlessly.')
 
-st.write("""
-Our AI-powered web scraper allows you to input any URL and specify exactly what content you're interested in extracting from that page.
+with placeholder:
+    st.title("Welcome")
+    st.write('This is an AI web scraper designed to assist you in extracting valuable information from websites effortlessly.')
 
-### Key Features:
-- **Flexible Input**: Enter any valid URL and specify your requirements through a customizable prompt.
-- **User-Friendly Interface**: Enjoy an intuitive design that makes it easy for anyone to use the web scraper.
-- **AI Integration**: Leverage the power of artificial intelligence to extract relevant information accurately.
+    st.write("""
+    Our AI-powered web scraper allows you to input any URL and specify exactly what content you're interested in extracting from that page.
 
-### How It Works:
-1. **Input URL**: Start by entering the URL of the website you wish to scrape.
-2. **Define Your Request**: In the prompt section, explain what specific information you would like to retrieve. Be as detailed as possible to get the best results.
-3. **Start the Scraping Process**: Click the "search" button, and the AI will handle the rest, pulling the desired data from the provided URL.
+    ### Key Features:
+    - **Flexible Input**: Enter any valid URL and specify your requirements through a customizable prompt.
+    - **User-Friendly Interface**: Enjoy an intuitive design that makes it easy for anyone to use the web scraper.
+    - **AI Integration**: Leverage the power of artificial intelligence to extract relevant information accurately.
 
-### Getting Started:
-To contribute, report issues, or explore the source code, please visit our project's GitHub repository: [GitHub - Scrape Wizard](https://github.com/hosseingz/ScrapeWizard)
+    ### How It Works:
+    1. **Input URL**: Start by entering the URL of the website you wish to scrape.
+    2. **Define Your Request**: In the prompt section, explain what specific information you would like to retrieve. Be as detailed as possible to get the best results.
+    3. **Start the Scraping Process**: Click the "search" button, and the AI will handle the rest, pulling the desired data from the provided URL.
 
-We are continually improving this project and welcome contributions from the community. Happy scraping!
-""")
+    ### Getting Started:
+    To contribute, report issues, or explore the source code, please visit our project's GitHub repository: [GitHub - Scrape Wizard](https://github.com/hosseingz/ScrapeWizard)
 
+    We are continually improving this project and welcome contributions from the community. Happy scraping!
+    """)
 
 url = st.text_input("URL", placeholder='Enter the URL you want to scrape')
 
-if prompt := st.chat_input("What do you want from this URL?"):
 
-    with st.chat_message("user"):
-        st.markdown(prompt)
+if url:
 
-    with st.spinner('Generating response...'):
-        
-        status, vectorstore, error = fetch_webpage(url)
-    
-        if status:
-             
-            response = ask_ai(prompt, vectorstore)
-            st.write_stream(stream_parser(response))
+    placeholder.empty()
 
-        else:
-            st.error(error)
+    if prompt := st.chat_input("What do you want from this URL?"):
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.spinner('Generating response...'):
+            status, vectorstore, error = fetch_webpage(url)
+
+            if status:
+                response = ask_ai(prompt, vectorstore)
+                st.write_stream(stream_parser(response))
+            else:
+                st.error(error)
